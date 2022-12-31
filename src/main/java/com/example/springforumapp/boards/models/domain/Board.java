e7ac92dd3b8a139222865ec74a319d9c41c5b32c
tofffee <tofffee012@gmail.com>
2022-12-31 18:58:21 +0300

    test v0.0.11.7.1

--- src/main/java/com/example/springforumapp/boards/models/domain/Board.java
+++ src/main/java/com/example/springforumapp/boards/models/domain/Board.java
@@ -2,12 +2,20 @@ package com.example.springforumapp.boards.models.domain;
 
 import com.example.springforumapp.publications.models.domain.Publication;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.List;
 
 @Entity
 @Table(name = "Boards")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Board {
     @Id
     @Column(name = "id")
@@ -19,35 +27,4 @@ public class Board {
 
     @OneToMany(mappedBy = "board")
     private List<Publication> publications;
-    public Board() {}
-
-    public Board(int id, String name, List<Publication> publications) {
-        this.id = id;
-        this.name = name;
-        this.publications = publications;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public List<Publication> getPublications() {
-        return publications;
-    }
-
-    public void setPublications(List<Publication> publications) {
-        this.publications = publications;
-    }
 }
--- src/main/java/com/example/springforumapp/boards/models/dto/BoardDTO.java
+++ src/main/java/com/example/springforumapp/boards/models/dto/BoardDTO.java
@@ -1,36 +1,22 @@
 package com.example.springforumapp.boards.models.dto;
 
 import com.example.springforumapp.publications.models.domain.Publication;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.List;
 
-
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class BoardDTO {
 
     private int id;
 
     private String name;
-    public BoardDTO() {}
 
-    public BoardDTO(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
