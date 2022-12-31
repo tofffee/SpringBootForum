2fc4f68505d7495003b5520680e56363da723de6
tofffee <tofffee012@gmail.com>
2022-12-31 18:58:47 +0300

    test v0.0.11.7.2

--- src/main/java/com/example/springforumapp/boards/models/dto/BoardDTO.java
+++ src/main/java/com/example/springforumapp/boards/models/dto/BoardDTO.java
@@ -1,18 +1,10 @@
 package com.example.springforumapp.boards.models.dto;
 
-import com.example.springforumapp.publications.models.domain.Publication;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-import javax.persistence.*;
-import java.util.List;
+import lombok.*;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
+
+@Data
 public class BoardDTO {
 
     private int id;
