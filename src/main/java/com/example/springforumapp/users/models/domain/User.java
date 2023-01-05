20214e98511ea4301325dee0a8268f266a06c226
tofffee <tofffee012@gmail.com>
2023-01-05 03:44:18 +0300

    test v0.11.13

--- src/main/java/com/example/springforumapp/users/models/domain/User.java
+++ src/main/java/com/example/springforumapp/users/models/domain/User.java
@@ -20,29 +20,37 @@ public class User {
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NotNull
     private int id;
 
     @Column(name = "username")
     @Size(min = 4 ,max = 15, message = "User error (username must be > 4 and < 15)")
+    @NotNull
     private String username;
 
     @Column(name = "avatar_url")
+    @NotNull
     private String avatarUrl;
 
     @Email
     @Column(name = "email")
+    @NotNull
     private String email;
 
     @Column(name = "password")
     @Size(min = 5 , message = "User error (Password must contain more than 5 symbols)")
+    @NotNull
     private String password;
 
     @Column(name = "role")
+    @NotNull
     private String role;
 
     @Column(name = "activation_code")
     private String activationCode;
+
     @Column(name = "enabled")
+    @NotNull
     private Boolean enabled;
 
     @OneToMany(mappedBy = "user")
